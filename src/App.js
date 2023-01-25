9a225e4785067581b3d1503466783ca35b0b93f3
Robin Andersson <robin.andersson@me.com>
2023-01-25 17:40:58 +0100

    test form for firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import handleSubmit from "./handlers/handleSubmit";
+import { useRef } from "react";
 
 function App() {
+  const dataRef = useRef();
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    handleSubmit(dataRef.current.value);
+    dataRef.current.value = "";
+  };
+
   return (
-    <Routes>
-      <Route></Route>
-    </Routes>
+    <div className="App">
+      <form onSubmit={submithandler}>
+        <input type="text" ref={dataRef} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
   );
 }
 
